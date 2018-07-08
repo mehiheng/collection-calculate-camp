d74eaf5fde8081ce775a8b83cae77fb0c403f6c8
mehiheng <38433021+mehiheng@users.noreply.github.com>
2018-07-09 00:29:20 +0800

    Update grouping_count.js

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -2,15 +2,15 @@
 
 function grouping_count(collection) {
   let result={};
-  for(let element of collection){
-    let index=collection.indexOf(element);
-    if(index===-1){
-      result[index]=1
-    }else{
-      result[index]+=1;
-    }
-  }
+	  for(let element of collection){
+		  if(result.hasOwnProperty(element)){
+			  result[element]+=1;
+		  }else{
+			  result[element]=1;
+		  }
+	  }
   return result;
 }
+
 
 module.exports = grouping_count;
