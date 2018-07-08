29489fc83b813f3d7bea9db1eccbe50ae73c6cac
mehiheng <38433021+mehiheng@users.noreply.github.com>
2018-07-09 00:46:55 +0800

    Update two_collections_practice_1.js

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+	let result=[];
+	for(let a of collection_a){
+		for(let b of collection_b){
+			if(a===b){
+				result.push(a);
+			}
+		}
+	}
+	return result;
 }
+
+
+
 
 module.exports = choose_common_elements;
