37742b9f0d5b8c854f0a7f2e0ee2cb8edb61090f
mehiheng <38433021+mehiheng@users.noreply.github.com>
2018-07-09 01:16:05 +0800

    Update two_collections_practice_3.js

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
-  //在这里写入代码
+	let result=[];
+	for(let a of collection_a){
+		for(let b of collection_b){
+			if(a%b===0){
+				result.push(a);
+				break;
+			}
+		}
+    }
+	return result;
 }
+
+
 
 module.exports = choose_divisible_integer;
